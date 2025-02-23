e91b6f344fbd9fbac18bdd4031fba4824373e54a
mostafanasser775 <alnasser401@gmail.com>
2025-02-23 23:25:16 +0200

    edited the videoplayer

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,23 +1,33 @@
 'use client';
 
 import { updateLessonCompleteStatus } from '@/features/lessons/actions/updateLessonStatusComplete';
-import { Suspense } from 'react';
+import { useRouter } from 'next/navigation';
+import { Suspense, useTransition } from 'react';
 import ReactPlayer from 'react-player';
 
 export function VideoPlayer({ videoUrl, lessonId }: { videoUrl: string, lessonId: string }) {
+    const [, startTransition] = useTransition();
+    const router = useRouter()
+    async function onvideoEnd() {
+        startTransition(async () => {
+            await updateLessonCompleteStatus(lessonId, true)
+            router.refresh()
+        });
+    }
     if (videoUrl && lessonId)
         return (
             <div className="flex justify-center  h-[600px] min-h-[500px] ">
                 <Suspense fallback={<div>...loading</div>} >
                     <ReactPlayer url={videoUrl} controls width='100%'
                         height='100%'
-                        onEnded={() => updateLessonCompleteStatus(lessonId, true)}
+
+                        onEnded={onvideoEnd}
                     />
 
                 </Suspense>
             </div>
         );
-        else return <div>
-            Loadng...
-        </div>
+    else return <div>
+        Loadng...
+    </div>
 }
